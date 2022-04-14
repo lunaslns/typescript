d41099e6f883d04e5747e3a2a9be33645cf22a3b
LDS <73805188+lunaslns@users.noreply.github.com>
2022-04-14 22:02:17 +0700

    update

---
+++ Lab2/dist/bai1.js
@@ -0,0 +1,14 @@
+"use strict";
+//su dung number, string, boolean
+let number1 = 5;
+let number2 = 2.8;
+let phrase = 'result is ';
+let permit = true;
+const result = number1 + number2;
+if (permit) {
+    console.log(phrase + result);
+}
+else {
+    console.log('not show result');
+}
+//# sourceMappingURL=bai1.js.map
---
+++ Lab2/src/bai1.ts
@@ -0,0 +1,11 @@
+//su dung number, string, boolean
+let number1:number = 5;
+let number2:number = 2.8;
+let phrase:string = 'result is ';
+let permit:boolean = true;
+const result = number1 + number2;
+if(permit){
+    console.log(phrase + result);
+}else{
+    console.log('not show result');
+}
