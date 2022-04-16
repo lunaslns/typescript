080515cd69c2bd21d030b25f2e3da7bc405d8931
LDS <73805188+lunaslns@users.noreply.github.com>
2022-04-16 21:32:05 +0700

    update

---
+++ Asm/dist/frontend/main.bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*****************************!*\
+  !*** ./src/frontend/App.ts ***!
+  \*****************************/
+
+console.log('frontend');
+
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
